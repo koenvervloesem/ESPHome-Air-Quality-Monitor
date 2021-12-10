de0730eb94d4f20be054b04dede9fbad0c0bdbe0
Koen Vervloesem <koen@vervloesem.eu>
2021-12-10 20:38:02 +0100

    Seperate functions to draw header, show calibration state and show connection state

--- include/display_utilities.cpp
+++ include/display_utilities.cpp
@@ -1,19 +1,23 @@
 #include "esphome.h"
 
-void drawHeader(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_header, esphome::Color color_ok, esphome::Color color_nok, esphome::Color color_info, esphome::switch_::Switch* co2_calibrate, esphome::binary_sensor::BinarySensor* co2_calibrated, esphome::binary_sensor::BinarySensor* system_status) {
-    // Draw header and frame
-    it.rectangle(0,  0, it.get_width(), it.get_height(), color_header);
-    it.rectangle(0, 20, it.get_width(), it.get_height(), color_header);
+// Draw header and frame
+void drawHeader(esphome::display::DisplayBuffer& it, esphome::Color color) {
+    it.rectangle(0,  0, it.get_width(), it.get_height(), color);
+    it.rectangle(0, 20, it.get_width(), it.get_height(), color);
+}
 
-    // Show calibration state at the left
+// Show calibration state at the left
+void showCalibration(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_info, esphome::Color color_nok, esphome::switch_::Switch* co2_calibrate, esphome::binary_sensor::BinarySensor* co2_calibrated) {
     if ((*co2_calibrate).state) {
         it.print(5, 5, font, color_info, esphome::display::TextAlign::TOP_LEFT, "Calibrating...");
     }
     else if (!(*co2_calibrated).state) {
         it.print(5, 5, font, color_nok, esphome::display::TextAlign::TOP_LEFT, "Calibrate");
     }
+}
 
-    // Show connection state at the right
+// Show connection state at the right
+void showStatus(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_ok, esphome::Color color_nok, esphome::binary_sensor::BinarySensor* system_status) {
     if ((*system_status).state) {
         it.print(it.get_width() - 5, 5, font, color_ok, esphome::display::TextAlign::TOP_RIGHT, "Online");
     }
--- include/display_utilities.h
+++ include/display_utilities.h
@@ -1,3 +1,5 @@
 #include "esphome.h"
 
-void drawHeader(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_header, esphome::Color color_ok, esphome::Color color_nok, esphome::Color color_info, esphome::switch_::Switch* co2_calibrate, esphome::binary_sensor::BinarySensor* co2_calibrated, esphome::binary_sensor::BinarySensor* system_status);
+void drawHeader(esphome::display::DisplayBuffer& it, esphome::Color color);
+void showCalibration(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_info, esphome::Color color_nok, esphome::switch_::Switch* co2_calibrate, esphome::binary_sensor::BinarySensor* co2_calibrated);
+void showStatus(esphome::display::DisplayBuffer& it, esphome::display::Font* font, esphome::Color color_ok, esphome::Color color_nok, esphome::binary_sensor::BinarySensor* system_status);
